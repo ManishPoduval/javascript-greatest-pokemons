cdcbde61424b017197cddab3935afcd19dd7bf92
Bernard Cardoso <be1.0@hotmail.com>
2023-04-12 07:36:38 +0100

    First Iteration done!

--- src/pokemons.js
+++ src/pokemons.js
@@ -1,9 +1,26 @@
+
 // Iteration 1: All Fire Pokemons- get the array of all Fire type pokemons
-
+function getAllFirePokemons(myPokemons){
+    const arrayOfPPokemons = myPokemons.filter(pokemon => pokemon.type === 'Fire');
+    return arrayOfPPokemons;
+};
+getAllFirePokemons(pokemons);
 // Iteration 2: Shortest Pokemon- find the `name` of the shortest pokemon
-
+function shortestPokemon(pokemons){
+    const minPokemon = Math.min(...pokemons.map(o => o.length));
+    for(let i = 0; i < pokemons.length; i++){
+        if(pokemons.length = 0){
+            return pokemons;
+        } else{
+            return `${minPokemon}`;
+        }
+    }
+}
+shortestPokemon(pokemons);
 // Iteration 3: candy_count average - average of `candy_count` for all the pokemons
+function candyAverage(){
 
+};
 // Iteration 4: images for the first 10 `Ground`  Pokemons
 
 // Iteration 5: Find all pokemon names heavier than Pikachu
