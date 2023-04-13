5577a9c22327f17c4bda352efdc84c7bba42d2ad
Luis Ramirez <rhluisenrique@gmail.com>
2023-04-12 21:20:01 -0400

    Pokemon Lab/ iterations 1-3... pending 4-7

--- src/pokemons.js
+++ src/pokemons.js
@@ -1,8 +1,32 @@
 // Iteration 1: All Fire Pokemons- get the array of all Fire type pokemons
-
+function getAllFirePokemons(array){
+    const firePokemon = array.filter(select => select.type.includes("Fire"))
+    return firePokemon;
+}
 // Iteration 2: Shortest Pokemon- find the `name` of the shortest pokemon
+function shortestPokemon(array){
+    if(array.length === 0){ return 0}
+    let shortest = array[0]
+    for(let i=1; i<array.length; i++){
+        if(array[i].height < shortest.height){
+            shortest = array[i]
+        }
+    } 
+    return shortest.name
+}
 
 // Iteration 3: candy_count average - average of `candy_count` for all the pokemons
+function candyAverage(array){
+    if(array.length === 0) return 0;
+    let total = 0;
+    let pokemons = array.length
+    for(let i=0; i< array.length; i++){
+        if(array[i].candy_count) 
+             total += array[i].candy_count    
+    }
+    let avg = (total/pokemons)
+    return Math.round(avg*100)/100;
+}
 
 // Iteration 4: images for the first 10 `Ground`  Pokemons
 
